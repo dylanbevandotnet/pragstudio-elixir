84d111fe36b302243b99df3fd03cdc53cc110042
Dylan Bevan <chimpfunk@live.ca>
2021-05-30 07:17:31 +0000

    Lesson 16

--- servy/lib/bear_controller.ex
+++ servy/lib/bear_controller.ex
@@ -3,23 +3,25 @@ defmodule Servy.BearController do
   alias Servy.Bear
   alias Servy.Wildthings
 
-  defp bear_item(%Bear{} = bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
+  @template_path Path.expand("../../templates", __DIR__)
+
+  defp render(conv, template, bindings \\ []) do
+    content =
+      @template_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+    %{ conv | resp_body: content }
   end
 
   def index(%Conv{} = conv) do
-    items = Wildthings.list_bears()
-            |> Enum.filter(&Bear.is_grizzly/1)
+    bears = Wildthings.list_bears()
             |> Enum.sort(&Bear.order_asc_by_name/2)
-            # equivalent of above, but a bit more readable
-            |> Enum.map(&bear_item(&1))
-            |> Enum.join
-    %{ conv | resp_body: "<ul>#{items}</ul>" }
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(%Conv{} = conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %{ conv | resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>" }
+    render(conv, "show.eex", bear: bear)
   end
 
   def create(%Conv{} = conv, %{"name" => name, "type" => type}) do
